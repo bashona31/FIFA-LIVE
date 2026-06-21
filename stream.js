730b44ea62bc5adabc6f4c05641b74f7a81cff8b
bashona31 <b.ashona3@gmail.com>
2026-06-21 16:02:38 +0600

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,6 +1,6 @@
 export const STREAM = {
-  title: "tunisia-vs-japan",
+  title: "spain-vs-saudi-arabia",
   league: "FIFA World Cup 2026",
   status: "LIVE",
-  embedUrl: "https://embed.st/embed/admin/ppv-tunisia-vs-japan/1"
+  embedUrl: "https://embed.st/embed/golf/23274/1"
 };
