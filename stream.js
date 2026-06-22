66ddeadfe082e3df0764cd68f1a26399c7822007
bashona31 <b.ashona3@gmail.com>
2026-06-22 23:08:20 +0600

    Update stream.js

--- stream.js
+++ stream.js
@@ -2,5 +2,5 @@ export const STREAM = {
   title: "belgium-iran-live",
   league: "FIFA World Cup 2026",
   status: "LIVE",
-  embedUrl: "https://embed.st/embed/delta/live_world-championship-gr-g_belgium-iran-live-streaming-1243724031/1"
+  embedUrl: "https://embed.st/embed/admin/ppv-argentina-vs-austria/1"
 };
