5712f0c0d9f90f06d09bfc36ad67c027e3101ce2
bashona31 <b.ashona3@gmail.com>
2026-06-22 23:38:49 +0600

    Update stream.js

--- stream.js
+++ stream.js
@@ -2,5 +2,5 @@ export const STREAM = {
   title: "Argentina vs Austria Player",
   league: "FIFA World Cup 2026",
   status: "LIVE",
-  embedUrl: "https://embed.st/embed/echo/argentina-vs-austria-football-1489399/2"
+  embedUrl: "https://embed.st/embed/golf/23301/2"
 };
