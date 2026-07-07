922b14d9b42d7bc0b00af37b691552fa93c00e44
bashona31 <b.ashona3@gmail.com>
2026-07-07 22:21:18 +0600

    Update stream.js

--- stream.js
+++ stream.js
@@ -2,5 +2,5 @@ export const STREAM = {
   title: "Scotland vs Brazil",
   league: "FIFA World Cup 2026",
   status: "LIVE",
-  embedUrl: "https://embed.st/embed/admin/ppv-scotland-vs-brazil/1"
+  embedUrl: "https://embed.st/embed/admin/ppv-argentina-vs-egypt/1"
 };
