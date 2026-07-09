bfe78a4446d2bb0ef30a4ce34533dd7518de2d49
bashona31 <b.ashona3@gmail.com>
2026-07-10 02:36:21 +0600

    Update stream.js

--- stream.js
+++ stream.js
@@ -2,5 +2,5 @@ export const STREAM = {
   title: "argentina-vs-egypt",
   league: "FIFA World Cup 2026",
   status: "LIVE",
-  embedUrl: "<iframe src="https://embed.cx/embed1/" width="1280" height="720" frameborder="0" allowfullscreen allow="autoplay; encrypted-media; picture-in-picture" scrolling="no"></iframe>"
+  embedUrl: "https://embed.cx/embed1/"
 };
