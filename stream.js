e8f005b47cce6e5606a4ccc69548cb55946fe82d
bashona31 <b.ashona3@gmail.com>
2026-07-10 02:31:55 +0600

    Update stream.js

--- stream.js
+++ stream.js
@@ -2,5 +2,5 @@ export const STREAM = {
   title: "argentina-vs-egypt",
   league: "FIFA World Cup 2026",
   status: "LIVE",
-  embedUrl: "https://embed.st/embed/admin/ppv-argentina-vs-egypt/1"
+  embedUrl: "https://embed.cx/embed1"
 };
